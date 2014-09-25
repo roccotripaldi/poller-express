478cf31886361344d405f7cdf59ef62a5a2586cf
Rocco Tripaldi <tripaldir@gmail.com>
2014-09-24 20:40:56 -0400

    let's try a small refactor

---
+++ controllers/poex_admin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Poller_express
+ */
+
+/**
+ * Our controller for the WordPress dashboard area - empty for now
+ * In step 2 we will fill it with properties and functions!
+ */
+class Poex_admin extends Poller_express {
+
+}
+
+?>
--- poller-express.php
+++ poller-express.php
@@ -18,18 +18,17 @@ define( 'POEX_DIR', dirname(__FILE__).'/' );
 
 /**
  * Our main controller - Empty for now
- * In the step 3 we will fill it with properties and methods
+ * In the step 2 we will fill it with properties and methods!
  */
 class Poller_express {
 
 }
 $poex = new Poller_express(); // let's get it started in here
 
-
 if( is_admin() ) {
     // if we are in the dashboard, let's get that started too!
-    require POEX_DIR . '/controllers/poex_settings.php';
-    $poex_settings = new Poex_settings();
+    require POEX_DIR . '/controllers/poex_admin.php';
+    $poex_admin = new Poex_admin();
 }
 
 ?>
