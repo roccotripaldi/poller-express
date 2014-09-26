979da2e18813508ce30fabaff7bbd39972644a20
Rocco Tripaldi <tripaldir@gmail.com>
2014-09-26 00:36:49 -0400

    let's show a really awesome chart

--- js/poex-results.js
+++ js/poex-results.js
@@ -1,5 +1,21 @@
-jQuery(document).ready(
-    function() {
+Chart.defaults.global.responsive = true;
 
-    }
-)
+var ctx = document.getElementById("poex_results").getContext("2d");
+var data = {
+    labels: poex_result_lables,
+    datasets: [
+        {
+            label: "Poll Results",
+            fillColor: "rgba(242,103,34,.8)",
+            strokeColor: "rgba(255,255,255,1)",
+            pointColor: "rgba(242,103,34,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(242,103,34,1)",
+            data: poex_result_totals
+
+        }
+    ]
+};
+console.log( poex_result_lables );
+var poex_results = new Chart(ctx).Line(data);
--- views/results.php
+++ views/results.php
@@ -1,5 +1,9 @@
 <div class="poex_poll">
-    <p>
-        This is a result
+    <h2 class="poex_title"><?php echo stripslashes( $settings['title'] ); ?></h2>
+    <p class="poex_question">
+        <?php echo $settings['question']; ?>
     </p>
+    <canvas id="poex_results" width="600px" height="200px">
+
+    </canvas>
 </div>
