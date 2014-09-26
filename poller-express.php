472148d5770a3eac8492726b6f4b303b5ac5acb3
Rocco Tripaldi <tripaldir@gmail.com>
2014-09-26 00:58:06 -0400

    adding a bunch of comments for step 3

--- poller-express.php
+++ poller-express.php
@@ -18,38 +18,52 @@ define( 'POEX_DIR', dirname(__FILE__).'/' );
 define( 'POEX_PATH', plugins_url() . '/poller-express/' );
 
 /**
- * Our main controller: has $settings which we can get with, viola!, the get_settings() method
+ * Our main controller creates a shortcode that will either render a voting poll, or the results of said poll
  */
 class Poller_express {
-    public $settings;
-    public $is_showing_shortcode;
-    public $is_showing_graph;
-    public $is_voting;
-    public $voting_error;
-    public $voting_message;
+    public $settings; // holds the user defined settings, or defaults
+    public $is_showing_shortcode; // will be true if the shortcode is rendering
+    public $is_showing_result; // will be true if we are showing voting results
+    public $is_voting; // will be true if the user is voting
+    public $voting_error; // will be true if there was a problem while voting
+    public $voting_message; // an error message
 
+    /**
+     * Our construct hooks internal methods to wordpress actions
+     */
     function __construct() {
+        // adds a new shortcode that can be used in any post content
         add_shortcode('poller_express', array( &$this, 'render_poll'));
+        // on  initialization, we'll let WordPress know where our relevent .js and .css files live
         add_action('init', array(&$this, 'register_poll_assets'));
+        // we'll link our .js and .css files in the footer
         add_action('wp_footer', array(&$this, 'print_scripts'));
+        // if we are submitting a vote, we can run that process during initialization
         if( isset( $_POST['action'] ) && $_POST['action'] == 'poex_vote' ) {
             add_action( 'init', array(&$this, 'vote') );
         }
     }
 
+    /**
+     * Validate and log any user submitted votes
+     */
     function vote() {
-
+        // using nonces to keep out trouble makers
         $nonce = isset( $_POST['poex_vote_nonce'] ) ? $_POST['poex_vote_nonce'] : '';
         if( ! wp_verify_nonce( $nonce, 'poex_vote' ) ) {
             return false;
         }
+
         $this->is_voting = true;
         $this->voting_error = true;
         $votes = isset( $_POST['poex_vote'] ) ? $_POST['poex_vote'] : array();
+        // let's make sure we have data before we process it
         if( empty($votes) || ! is_array( $votes ) ) {
             $this->voting_message = 'You didn\'t cast a vote chief';
             return false;
         }
+
+        // save the vote to the wp_options table
         $this->voting_error = false;
         $existing_votes = get_option( 'poex_votes', array() );
         foreach( $votes as $v ) {
@@ -63,6 +77,9 @@ class Poller_express {
         update_option( 'poex_votes', $existing_votes );
     }
 
+    /**
+     * We'll print our scripts in the footer only if we need them
+     */
     function print_scripts() {
         if( $this->is_showing_shortcode === true ) {
             wp_enqueue_style( 'poex-css' );
@@ -76,6 +93,10 @@ class Poller_express {
         }
     }
 
+    /**
+     * Grabs all votes compares them to the current settings, and returns the relevent votes
+     * @return array
+     */
     function get_votes() {
         $settings = $this->get_settings();
         $votes = get_option( 'poex_votes', array() );
@@ -94,6 +115,9 @@ class Poller_express {
         return $return;
     }
 
+    /**
+     * Here's where we declare our .js and .css files
+     */
     function register_poll_assets() {
         wp_register_script( 'charts-js', POEX_PATH . 'js/charts.js' );
         $results_dependencies = array( 'jquery', 'charts-js' );
@@ -101,6 +125,10 @@ class Poller_express {
         wp_register_style( 'poex-css', POEX_PATH . 'css/poex.css' );
     }
 
+    /**
+     * Here's our short code processor, will either render a poll or the poll results
+     * @return string
+     */
     function render_poll() {
         $this->is_showing_shortcode = true;
         $settings = $this->get_settings();
--- views/results.php
+++ views/results.php
@@ -3,6 +3,7 @@
     <p class="poex_question">
         <?php echo $settings['question']; ?>
     </p>
+    <!-- our canvas element will be populated by our awesome charts.js -->
     <canvas id="poex_results" width="600px" height="200px">
 
     </canvas>
